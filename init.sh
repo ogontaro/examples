6dc2f1af7d074583e8851e417dc3e278c81a7218
ogontaro <ogontaro@gmail.com>
2024-01-27 21:37:37 +0900

    Add .gitignore file and init script
    A .gitignore file has been added to exclude kubernetes, helm-chart, and kustomize directories from the repository. Additionally, an initialization script (init.sh) is introduced to streamline the cloning of these directories if they do not already exist locally.

---
+++ init.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -eu
+
+if [ ! -d ./kubernetes ] ; then
+  git clone git@github.com:ogontaro/examples_kubernetes kubernetes
+fi
+
+if [ ! -d ./helm-chart ] ; then
+  git clone git@github.com:ogontaro/examples_helm-chart helm-chart
+fi
+
+if [ ! -d ./kustomize ] ; then
+  git clone git@github.com:ogontaro/examples_kustomize kustomize
+fi
